08a69d325a6691dbca6f23ec3d774b1e507aa0e4
Walter Lee Davis <waltd@wdstudio.com>
2016-02-05 11:45:35 -0500

    fixed quotes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
   
-  config.secret_key_base = ENV[“SECRET_KEY_BASE”]
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 
 
   # Prepend all log lines with the following tags.
